253bf98a4d9e30afca0ecaee247cc617b6bb02b7
Nishkala10 <nishkala.krishnamurthy@gmail.com>
2025-10-15 20:00:08 +0530

    All files

---
+++ Day6/template/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ProfileCard from "./components/ProfileCard";
+import List from "./components/List";
+import Form from "./components/form";
+
+function App() {
+  const [activeTab, setActiveTab] = useState("profile");
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Components Demo</h1>
+
+      {/* Nav Tabs */}
+      <div style={{ marginBottom: "20px" }}>
+        <button onClick={() => setActiveTab("profile")}>Profile</button>
+        <button onClick={() => setActiveTab("list")}>List</button>
+        <button onClick={() => setActiveTab("form")}>Form</button>
+
+      </div>
+
+      {/* Tab Content */}
+      <div>
+        {activeTab === "profile" && <ProfileCard />}
+        {activeTab === "list" && <List />}
+        {activeTab === "form" && <Form />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
